2954e535ad753b2d60d650c62b7247981951b9ac
karenJustcode <karen.justcode.llc@gmail>
2022-03-24 18:01:07 +0400

    mmmmmmmmmmmmmmmmmmm

--- public/js/script.js
+++ public/js/script.js
@@ -709,7 +709,7 @@ $(document).on("submit", ".update_place_an_ad", function (event) {
     formData.append('address', address_val);
 
     $.ajax({
-        url: "http://185.46.11.159/BowyLaravel/public/announcement",
+        url: "http://185.46.11.159/BowyLaravel/public/announcement/",
         type: 'POST',
         data: formData,
         cache: false,
