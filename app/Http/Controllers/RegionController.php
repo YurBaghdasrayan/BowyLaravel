f1e6bfb30f527dd8a0cbd0b04e583ac7e7ded8b1
karenJustcode <karen.justcode.llc@gmail>
2022-04-08 16:03:43 +0400

    dd

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -9,7 +9,7 @@ class RegionController extends Controller
 {
     public function getCityByRegionId(Request $request)
     {
-        dd($request);
+//        dd($request);
         $region_id = $request->region_data;
         $regions = City::where('region_id', '=', $region_id)->get();
         return response()->json(['city_data' => $regions]);
