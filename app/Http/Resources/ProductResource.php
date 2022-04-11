efcd52c70eb86c42c232670e71967d59ff628238
karenJustcode <karen.justcode.llc@gmail>
2022-04-11 11:09:22 +0400

    asdasdasd

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -9,12 +9,12 @@ class ProductResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        $return =  parent::toArray($request);
+        $return = parent::toArray($request);
         $return += [
             'City' => $this->resource->City,
             'Region' => $this->resource->Region,
@@ -23,13 +23,13 @@ class ProductResource extends JsonResource
             "description" => $this->description,
             "body_type" => $this->body_type,
             "rudder" => $this->rudder,
-            "year_of_issue" =>$this->year_of_issue,
-            "transmission" =>$this->transmission,
-            "status" =>$this->status,
-            "address" =>$this->address,
+            "year_of_issue" => $this->year_of_issue,
+            "transmission" => $this->transmission,
+            "status" => $this->status,
+            "address" => $this->address,
             "Views" => count($this->resource->Views),
             "Call" => count($this->resource->Call),
-            "image"=>$this->resource->image
+            "image" => $this->resource->image
         ];
         return $return;
     }
