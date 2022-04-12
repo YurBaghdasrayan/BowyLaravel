dad23751971fe090e044bd22793e6b2fcf4ee0d8
karenJustcode <karen.justcode.llc@gmail>
2022-04-12 18:44:07 +0400

    asdasdasd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -286,7 +286,6 @@
             </div>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
             @if(isset($product))
-{{--            {{$product->total(4)}}--}}
                 {{$product->links()}}
             @endif
         </section>
