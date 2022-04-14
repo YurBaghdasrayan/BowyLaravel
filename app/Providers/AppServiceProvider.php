69c2a43f0fc1df8c6f7524fcd24ecff472f585f9
karenJustcode <karen.justcode.llc@gmail>
2022-04-14 17:36:54 +0400

    asdasd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        if (env('APP_ENV') !== 'local')
+        if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
+        }
+
     }
 }
